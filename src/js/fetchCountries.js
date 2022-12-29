171c727e86b3dcb20f39cb6df33ec54f820b8215
Ruslan3571 <ruslan12032@gmail.com>
2022-12-29 21:22:52 +0200

    +

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,7 @@
-export const fetchCountries = name => {
+const BAS_URL = 'https://restcountries.com';
+const fetchCountries = name => {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
+    `${BAS_URL}/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
   )
     .then(response => {
       if (!response.ok) {
@@ -15,3 +16,4 @@ export const fetchCountries = name => {
       console.error(error);
     });
 };
+export { fetchCountries };
